95d447dde1d8776263c3f0f0f06895196741bbb2
Robin van der Knaap <robin.vanderknaap@gmail.com>
2024-09-28 19:18:36 +0200

    Now we have a fully functional website

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+const colors = require('tailwindcss/colors')
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './hugo_stats.json',
+    './assets/js/*.js'
+  ],
+  plugins: [
+    require('@tailwindcss/typography'),
+  ],
+  theme: {
+    extend: {
+      colors: {
+        accent: colors.pink,
+      },
+      fontFamily: {
+        'righteous': ['Righteous']
+      }
+    },
+  },
+  plugins: [],
+}
